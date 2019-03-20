6994afa4cb12d2eb6a46e3ecce3c01aaa03b9b8d
madalenamarcelino <madelynbennt@gmail.com>
2019-03-20 18:51:54 +0000

    string

---
+++ solucoes/02_bases_cs/Bases/Ex02.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex02._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira uma frase");
+            string woops = Console.ReadLine();
+
+            Console.WriteLine(woops);
+        }
+    }
+}
