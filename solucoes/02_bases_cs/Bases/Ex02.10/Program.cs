47801216da718f5c23a7686e24c5776344ce7e12
madalenamarcelino <madelynbennt@gmail.com>
2019-03-20 18:58:52 +0000

    exercicio 10 de bases

--- solucoes/02_bases_cs/Bases/Ex02.10/Program.cs
+++ solucoes/02_bases_cs/Bases/Ex02.10/Program.cs
@@ -6,9 +6,11 @@ namespace Ex02._10
     {
         static void Main(string[] args)
         {
+            //asking for a string
             Console.WriteLine("Insira uma frase");
+            // naming and reading the string
             string woops = Console.ReadLine();
-
+            //showing string
             Console.WriteLine(woops);
         }
     }
